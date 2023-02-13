ad3f9fbac2e558541cab7bdebc358089da77d57f
Tamunokuro <tamunokrobluejack@gmail.com>
2023-02-13 19:32:07 +0100

    Create classes

---
+++ teacher.rb
@@ -0,0 +1,14 @@
+require './person.'
+
+class Teacher < Person
+  attr_accessor :specialization
+
+  def initialize(specialization, age, name = 'Unknown', id = Random.rand(1..1000), parent_permission: true)
+    super(age, name, id, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
