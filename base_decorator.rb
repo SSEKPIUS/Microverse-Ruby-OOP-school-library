bc864fd473a41fe29d1dc9be6d5f1974127a3710
Tamunokuro <tamunokrobluejack@gmail.com>
2023-02-14 17:12:05 +0100

    Edit files

--- base_decorator.rb
+++ base_decorator.rb
@@ -1,4 +1,4 @@
-require './nameable'
+require_relative 'nameable'
 
 class Decorator < Nameable
   attr_accessor :nameable
@@ -9,6 +9,6 @@ class Decorator < Nameable
   end
 
   def correct_name
-    @nameable.correct_name
+    @nameable
   end
 end
