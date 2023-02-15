b4dbcb192a6adc860c0c65c4098113e0b1074421
Tamunokuro <tamunokrobluejack@gmail.com>
2023-02-15 03:04:42 +0100

    Make required changes

--- person.rb
+++ person.rb
@@ -1,4 +1,34 @@
-require_relative 'nameable'
+class Nameable
+  def correct_name
+    raise NotImplementedError
+  end
+end
+
+class Decorator < Nameable
+  attr_accessor :nameable
+
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    name = @nameable.correct_name
+    name.length > 10 ? name[0..9] : name
+  end
+end
 
 class Person < Nameable
   attr_reader :id
