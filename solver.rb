a9bada9e187981b67735b79b17bc97ecb89404eb
pius ssekweyama <ssekweyamapius@gmail.com>
2023-02-24 09:39:48 +0300

    fix linters

--- solver.rb
+++ solver.rb
@@ -1,9 +1,9 @@
 class Solver
-  def factorial(n)
-    raise ArgumentError, "Argument must be a non-negative integer" unless n.is_a?(Integer) && n >= 0
+  def factorial(number)
+    raise ArgumentError, 'Argument must be a non-negative integer' unless number.is_a?(Integer) && number >= 0
 
     result = 1
-    (1..n).each { |i| result *= i }
+    (1..number).each { |i| result *= i }
     result
   end
 
@@ -11,15 +11,15 @@ class Solver
     word.reverse
   end
 
-  def fizzbuzz(n)
-    if n % 3 == 0 && n % 5 == 0
-      "fizzbuzz"
-    elsif n % 3 == 0
-      "fizz"
-    elsif n % 5 == 0
-      "buzz"
+  def fizzbuzz(number)
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
     else
-      n.to_s
+      number.to_s
     end
   end
 end
--- spec/Solver_spec.rb
+++ spec/Solver_spec.rb
@@ -7,22 +7,22 @@ describe Solver do
     expect(solver.factorial(0)).to eq 1
     expect(solver.factorial(1)).to eq 1
     expect(solver.factorial(5)).to eq 120
-    expect { solver.factorial(-1) }.to raise_error(ArgumentError, "Argument must be a non-negative integer")
-    expect { solver.factorial(1.5) }.to raise_error(ArgumentError, "Argument must be a non-negative integer")
-    expect { solver.factorial(nil) }.to raise_error(ArgumentError, "Argument must be a non-negative integer")
-    expect { solver.factorial("hello") }.to raise_error(ArgumentError, "Argument must be a non-negative integer")
+    expect { solver.factorial(-1) }.to raise_error(ArgumentError, 'Argument must be a non-negative integer')
+    expect { solver.factorial(1.5) }.to raise_error(ArgumentError, 'Argument must be a non-negative integer')
+    expect { solver.factorial(nil) }.to raise_error(ArgumentError, 'Argument must be a non-negative integer')
+    expect { solver.factorial('hello') }.to raise_error(ArgumentError, 'Argument must be a non-negative integer')
   end
 
   #   it 'Should test reverse' do
-  #     expect(solver.reverse("hello")).to eq "olleh"
-  #     expect(solver.reverse("")).to eq ""
-  #     expect(solver.reverse("racecar")).to eq "racecar"
+  #     expect(solver.reverse('hello')).to eq 'olleh'
+  #     expect(solver.reverse('')).to eq ''
+  #     expect(solver.reverse('racecar')).to eq 'racecar'
   #   end
 
   #   it 'Should test fizzbuzz' do
-  #     expect(solver.fizzbuzz(15)).to eq "fizzbuzz"
-  #     expect(solver.fizzbuzz(9)).to eq "fizz"
-  #     expect(solver.fizzbuzz(10)).to eq "buzz"
-  #     expect(solver.fizzbuzz(7)).to eq "7"
+  #     expect(solver.fizzbuzz(15)).to eq 'fizzbuzz'
+  #     expect(solver.fizzbuzz(9)).to eq 'fizz'
+  #     expect(solver.fizzbuzz(10)).to eq 'buzz'
+  #     expect(solver.fizzbuzz(7)).to eq '7'
   #   end
 end
